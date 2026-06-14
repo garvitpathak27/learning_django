4d05357dfb85653001fa3936be18835afae8e388
garvitpathak27 <garvitpathak2003@gmail.com>
2026-06-14 17:54:03 +0530

    initial commit

---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path , include 
+from django.contrib import admin 
+
+
+
+from .views import HomePageView
+
+urlpatterns = [
+        path('',HomePageView.as_view() , name = 'home'),
+        ]
+
+
