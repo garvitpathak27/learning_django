792ec57598c1039d7cd9e19801753a83aa112e00
garvit <garvit.p@ninestars.in>
2026-06-18 16:05:45 +0530

    user account

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -20,4 +20,7 @@ from django.urls import path , include
 urlpatterns = [
     path('',include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    
 ]
